0f515e988516d7cd8104c3ec70c73f126e29af07
DimaLitvinenko <dimaly.too@gmail.com>
2022-11-26 15:10:51 +0200

    index files changed

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from '../components/Header/Header';
+import { Footer } from '../components/Footer/Footer';
+import { Content } from '../components/Content/Content';
+
+export const Instagram = () => {
+    return (
+        <>
+            <Header />
+            <Content />
+            <Footer />
+        </>
+    );
+};
